5645f4f51cf531454b07747c7c6da7634e79dc28
Andrew Odewahn <odewahn@oreilly.com>
2014-12-05 10:31:10 -0500

    tweaking start.sh

--- start.sh
+++ start.sh
@@ -19,11 +19,14 @@ export TOKEN=48a36627afb087654f7e8de31f9a0780a23bc416b0b9c9d3412abd53a1ab
 
 docker run --net=host -e CONFIGPROXY_AUTH_TOKEN=$TOKEN --name=proxy jupyter/configurable-http-proxy --default-target http://192.168.59.103:9999
 
-docker run --net=host  \
-   -e CONFIGPROXY_AUTH_TOKEN=$TOKEN \
+
+
+
+docker run --net=host -e CONFIGPROXY_AUTH_TOKEN=$TOKEN \
    -v /var/run/docker.sock:/docker.sock \
    jupyter/tmpnb \
-   python orchestrate.py --container_ip=0.0.0.0 --image="odewahn/jupyter-kernel" --command="./go.sh" --pool_size=5
+   python orchestrate.py --container_ip=0.0.0.0 --image="odewahn/jupyter-kernel-test2" \
+   --command="python jupyter-kernel.py --base_path='{base_path}'" --pool_size=5 --redirect-uri="/"
 
 
 
